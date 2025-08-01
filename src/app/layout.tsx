a99b726b804d1c24ab2a973395adddd61250ed2f
Tai Porto <tai.dporto@gmail.com>
2025-08-01 18:49:09 -0300

    chore: setup project

---
+++ src/app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import localFont from 'next/font/local'
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Clever Photos",
+  description: "Clever Photos is a photo sharing platform",
+};
+
+const helvetica = localFont({
+  src: [
+    {
+      path: '../../public/fonts/Helvetica-Bold.ttf',
+      weight: '700',
+      style: 'bold'
+    },
+    {
+      path: '../../public/fonts/Helvetica.ttf',
+      weight: '400',
+      style: 'normal'
+    }
+  ]
+})
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${helvetica.className} antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,9 @@
+module.exports = {
+    theme: {
+      extend: {
+        fontFamily: {
+          helvetica: ['var(--font-helvetica)', 'sans-serif'],
+        },
+      },
+    },
+  };
